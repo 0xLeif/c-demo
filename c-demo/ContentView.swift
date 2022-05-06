67be1b7e7200d1c6c8c928a134a9ac0589d33e03
0xLeif <zmeriksen@icloud.com>
2022-05-05 20:51:15 -0600

    Update to demo scoping

--- c-demo/ContentView.swift
+++ c-demo/ContentView.swift
@@ -9,21 +9,32 @@ import c
 import CacheStore
 import SwiftUI
 
+enum ContentViewKey {
+    case value
+    case emoji
+}
+
 struct ContentView: View {
-    @ObservedObject var cacheStore: CacheStore<CacheKey> = c.resolve("CacheStore")
+    @ObservedObject var cacheStore: CacheStore<ContentViewKey> = c.resolve("ContentCacheStore")
     
     var stringValue: String {
-        cacheStore.resolve(.someValue)
+        cacheStore.resolve(.emoji)
     }
     
     var body: some View {
         VStack {
-            Text("Current Value: \(stringValue)")
+            Text("Current Emoji: \(stringValue)")
             Button("Update Value") {
-                cacheStore.set(value: ":D", forKey: .someValue)
+                cacheStore.set(value: ":D", forKey: .value)
+            }
+            
+            if let value: String = cacheStore.get(.value) {
+                Text("Value: \(value)")
             }
         }
+        .background(Color.gray)
         .padding()
+        
     }
 }
 
@@ -31,7 +42,7 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView(
             cacheStore: CacheStore(
-                initialValues: [.someValue: "Preview Cache Value"]
+                initialValues: [:]
             )
         )
     }
--- c-demo/c_demoApp.swift
+++ c-demo/c_demoApp.swift
@@ -10,20 +10,53 @@ import CacheStore
 import SwiftUI
 
 enum CacheKey: Hashable {
+    case valueToChange
     case someValue
+    case someOtherValue
 }
 
 @main
 struct DemoApp: App {
     @StateObject var cacheStore = CacheStore<CacheKey>(
-        initialValues: [.someValue: "🥳"]
+        initialValues: [
+            .someValue: "🥳",
+            .someOtherValue: "UH OH!"
+        ]
     )
     
     var body: some Scene {
         c.set(value: cacheStore, forKey: "CacheStore")
         
+        let contentCacheStore: CacheStore<ContentViewKey> = cacheStore.scope(
+            keyTransformation: c.transformer(
+                from: { cacheKey in
+                    switch cacheKey {
+                    case .valueToChange: return .value
+                    case .someValue: return .emoji
+                    default: return nil
+                    }
+                },
+                to: { contentViewKey in
+                    switch contentViewKey {
+                    case .value: return .valueToChange
+                    case .emoji: return .someValue
+                    default: return nil
+                    }
+                }
+            )
+        )
+        c.set(value: contentCacheStore, forKey: "ContentCacheStore")
+        
         return WindowGroup {
             VStack {
+                if let value: String = cacheStore.get(.valueToChange) {
+                    Text(value)
+                        .font(.largeTitle)
+                    
+                    Button("Remove") {
+                        cacheStore.set(value: NSNull(), forKey: .valueToChange)
+                    }
+                }
                 Text("@StateObject value: \(cacheStore.resolve(.someValue) as String)")
                 ContentView()
             }
